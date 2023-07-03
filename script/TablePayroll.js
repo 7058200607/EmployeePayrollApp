7c6c18e1d4457e4f5a39eb3a451305f70658ee16
Vrushali Joshi <vrushalijoshi589@gmail.com>
2023-07-03 16:11:18 +0530

    Delete data from emplopyee payroll app

---
+++ script/TablePayroll.js
@@ -0,0 +1,55 @@
+$(document).ready( function(){
+$.ajax({
+    type: "GET",
+    dataType: "json",
+    url: ("http://localhost:3000/Data"),
+    data: {get_param:'value'},
+    success: function (data,textstatus,xhr) {
+        console.log(data);
+        var trHTML = ""
+        $.each(data, function (i,item) {
+        var Department1=JSON.parse(item.Department)
+
+       trHTML+=`<tr>
+       <td><img style="height:20px; width:20px ;border:2px solid white" src="${item.profileImage}" alt=""> <span>${item.Name}</span>
+        </td>
+       <td> ${item.Gender}</td>
+       <td > ${Department1}</td>
+       <td> ${item.Salary}</td>
+       <td> ${item.startDate}</td>
+       <td>
+         <i class="fa-solid fa-trash-can" onclick="Delete(${item.id})"></i> </button>
+       <button class="UpdateButton" type="button" onclick="Onsubmmit()"> <i class="fa-sharp fa-solid fa-pen"></i> </button>
+
+       </td>
+    
+       </tr>`
+        console.log(Department1)
+        });
+        $('.Tbody').append(trHTML);
+        alert("Data Get Sucessfully")
+    console.log(data);
+
+    },
+    Error: function (xhr,textstatus,errorTrown) {
+        alert("Posting in error")
+    }
+})
+});
+
+function Delete(id){
+console.log(id,"bfhfcfdcfcgc")
+ $.ajax({
+    type: "DELETE",
+    dataType: "json",
+    url: `http://localhost:3000/Data/${id}`,  
+    success: function (data,textstatus,xhr) {
+        console.log(data);
+        alert("Data Delete sucessfully")
+    },
+
+    Error: function (xhr,textstatus,errorTrown) {
+        alert("Data not Deleted")
+    }    
+ })
+ }
--- script/employeeform.js
+++ script/employeeform.js
@@ -1,41 +1,57 @@
-function Onsubmmit(){
-   // console.log("save data");
-const Name=$('.nameinput').val()
-const Department=[];
-const profileImage=$("input[name=profileimage]:checked").val()
-const Gender=$("input[name=Gender]:checked").val()
- $("input[name=Department]:checked").each(
-    function(){
-        Department.push($(this).val())
+function Onsubmmit() {
+     console.log("save data");
+    const Name = $('.nameinput').val()
+    const Department = [];
+    const profileImage = $("input[name=profileimage]:checked").val()
+    const Gender = $("input[name=Gender]:checked").val()
+    $("input[name=Department]:checked").each(
+        function () {
+            Department.push($(this).val())
+        }
+    ) 
+    //const salary = document.getElementById("#salary").value;
+    const Salary = $("#Salary").val()
+    const Date = $("#date").val()
+    const Month = $(".month").val()
+    const Year = $(".Year").val()
+    const Notes = $(".noteinput").val()
+    let reqdata = {
+        Name: Name,
+        profileImage: profileImage,
+        Gender: Gender,
+        Department: JSON.stringify(Department),
+        Salary: Salary,
+        startDate: Date + "/" + Month + "/" + Year,
+        Notes: Notes
     }
-)
-const Salary=$("#Salary").val()
-const Date=$("#date").val()
-const Month=$(".month").val()
-const Year=$(".Year").val()
-const Notes=$(".noteinput").val()
+    $.ajax({
+        type: "POST",
+        url: " http://localhost:3000/Data",
+        data: reqdata,
+        success: function () {
+            alert("Data Post Sucessfully")
+        },
+        Error: function () {
+            alert("Posting in error")
+        }
 
-let reqdata={
-        Name:Name,
-        profileImage:profileImage,
-        Gender:Gender,
-        Department:JSON.stringify(Department),
-        Salary:Salary,
-       startDate:Date+"/"+ Month +"/"+Year,
-        Notes:Notes
-    }
-$.ajax({ 
-    type:"POST",
-    url:" http://localhost:3000/Data",
-    data:reqdata,
-    success:function(){
-        alert("Data Post Sucessfully")
-    },
-    Error:function(){
-        alert("Posting in error")
-    }
-
-})
+    })
     console.log(reqdata);
 }
 
+
+
+
+
+
+
+
+//  function Display(a,b){
+// return new Promise((resolve, reject) => {
+//         console.log();
+//     })
+//  }
+//  Display(5,10)
+//  .then(){
+//     console.log(result)
+//  }
